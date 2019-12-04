c6722e8daf13c29b7e4f1a856e055ebd6a4a26f4
Guido Weber <guido.weber0815@googlemail.com>
2019-12-04 20:51:51 +0100

    Day 4 part 2

--- day04.clj
+++ day04.clj
@@ -19,3 +19,20 @@
         (count)))
 
 (println "Part 1:" (part1))
+
+(defn criteria2? [n]
+    (let [[a,b,c,d,e,f] (map (fn [x] (int x)) (str n))]
+      (and
+          (<= a b)
+          (<= b c)
+          (<= c d)
+          (<= d e)
+          (<= e f)
+          (>= (count (filter #(= (second %) 2) (frequencies (str n)))) 1))))
+
+(defn part2 []
+    (->> (range 138307 654505)
+        (filter criteria2?)
+        (count)))
+
+(println "Part 2:" (part2))
