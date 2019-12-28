2b218eb5e98f706bd22a1a6f5afbfeed9282b0d4
Guido Weber <guido.weber0815@googlemail.com>
2019-12-28 12:07:06 +0100

    Move asserts to unit tests

--- src/aoc_2019/day_01/core.clj
+++ src/aoc_2019/day_01/core.clj
@@ -12,15 +12,6 @@
 (defn fuel [m]
     (- (int (/ m 3)) 2))
 
-;    For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-;    For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-;    For a mass of 1969, the fuel required is 654.
-;    For a mass of 100756, the fuel required is 33583.
-(assert (= (fuel 12) 2))
-(assert (= (fuel 14) 2))
-(assert (= (fuel 1969) 654))
-(assert (= (fuel 100756) 33583))
-
 (defn part1 []
     (->> (inp)
         (map fuel)
@@ -32,19 +23,6 @@
       (+ f (rfuel2 (fuel f)))))
 
 (defn fuel2 [m] (rfuel2 (fuel m)))
-
-; A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided
-; by 3 and rounded down is 0, which would call for a negative fuel), so the total
-; fuel required is still just 2.
-; At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216
-; more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel,
-; which requires 5 fuel, which requires no further fuel. So, the total fuel required
-; for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
-; The fuel required by a module of mass 100756 and its fuel is:
-; 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
-(assert (= (fuel2 14) 2))
-(assert (= (fuel2 1969) 966))
-(assert (= (fuel2 100756) 50346))
 
 (defn part2 []
     (->> (inp)
